969ea416aa0dfaea2eb74c19e40375e14b3fb4bc
Miguel Ángel Fernández Fernández <miguelangelprogramacion@gmail.com>
2015-08-21 10:21:21 +0200

    Basic Spring Configuration added

--- src/main/java/world/we/deserve/App.java
+++ src/main/java/world/we/deserve/App.java
@@ -1,13 +1,28 @@
 package world.we.deserve;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
 /**
- * Hello world!
+ * Launcher
  *
  */
+@Component
 public class App 
 {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ApplicationContext context = new AnnotationConfigApplicationContext(StormCassandraComponentScan.class);    	
+    	App app = context.getBean(App.class);
+    	app.greeting();
     }
+
+	private void greeting() {		
+		LOG.info("Greeting");
+	}
 }
---
+++ src/main/java/world/we/deserve/StormCassandraComponentScan.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package world.we.deserve;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Miguel Ángel Dev (miguelangelprogramacion@gmail.com)
+ *
+ */
+@Configuration
+@ComponentScan(basePackages = { "world.we.deserve" })
+public class StormCassandraComponentScan {
+
+}
