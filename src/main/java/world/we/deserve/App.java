f374ac3705c78241056f00c971faf61638dbf75e
miguelangelprogramacion <miguelangelprogramacion@gmail.com>
2015-08-21 19:49:37 +0200

    Added a trident topology. It contains a spout to emmit lines from a file, and a bolt to print the content of every parsed line

--- src/main/java/world/we/deserve/App.java
+++ src/main/java/world/we/deserve/App.java
@@ -4,15 +4,22 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 import backtype.storm.Config;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
 import backtype.storm.LocalCluster;
 import backtype.storm.StormSubmitter;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.AlreadyAliveException;
 import backtype.storm.generated.AuthorizationException;
+import backtype.storm.generated.AuthorizationException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -21,78 +28,74 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 import backtype.storm.utils.Utils;
-
+import backtype.storm.utils.Utils;
+import storm.trident.Stream;
+import storm.trident.Stream;
+import storm.trident.TridentTopology;
+import storm.trident.TridentTopology;
+import world.we.deserve.bolt.function.PrintTuple;
+import world.we.deserve.spout.LoadFileData;
 /**
  * Launcher
  *
  */
 @Component
-public class App 
-{
-	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+public class App {
 	
-    public static void main( String[] args )
-    {
-    	ApplicationContext context = new AnnotationConfigApplicationContext(StormCassandraComponentScan.class);    	
-    	App app = context.getBean(App.class);
-    	app.greeting(args);
-    }
+	@Autowired
+	Config tridentConfig;
+	
+	@Autowired
+	TridentTopology topology;
 
-	private void greeting(String[] args) {		
-		LOG.info("Greeting");
-		
-		TopologyBuilder builder = new TopologyBuilder();
+	@Autowired
+	LoadFileData loadFileDataSpout;
+	
+	@Autowired
+	PrintTuple printBolt;
 
-		builder.setSpout("word", new TestWordSpout(), 10);
-		builder.setBolt("exclaim1", new ExclamationBolt(), 3).shuffleGrouping("word");
-		builder.setBolt("exclaim2", new ExclamationBolt(), 2).shuffleGrouping("exclaim1");
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-		Config conf = new Config();
-		conf.setDebug(true);
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(StormCassandraComponentScan.class);
+		App app = context.getBean(App.class);
+//		app.greeting(args);
 
+		String host = null;
 		if (args != null && args.length > 0) {
-			conf.setNumWorkers(3);
+			host = args[0];
+		}
+		app.tridentLaunch(host);
+
+	}
+
+	private void tridentLaunch(String host) {
+
+		Stream fileStream = topology.newStream("test", loadFileDataSpout);		
+		fileStream.each(new Fields("test"), printBolt, new Fields());
+
+		if (host != null) {
 
 			try {
-				StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+				// Remember (limited config)
+				StormSubmitter.submitTopology(host, tridentConfig, topology.build());
 			} catch (AlreadyAliveException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (InvalidTopologyException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (AuthorizationException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} else {
-
 			LocalCluster cluster = new LocalCluster();
-			cluster.submitTopology("test", conf, builder.createTopology());
-			Utils.sleep(10000);
-			cluster.killTopology("test");
+			cluster.submitTopology("topology", tridentConfig, topology.build());
+			Utils.sleep(1000 * 60 * 300);
+			cluster.killTopology("topology");
 			cluster.shutdown();
 		}
-	}
-	
-	public static class ExclamationBolt extends BaseRichBolt {
-		OutputCollector _collector;
-
-		public void prepare(Map conf, TopologyContext context, OutputCollector collector) {
-			_collector = collector;
-		}
-
-		public void execute(Tuple tuple) {
-			_collector.emit(tuple, new Values(tuple.getString(0) + "!!!"));
-			_collector.ack(tuple);
-		}
-
-		public void declareOutputFields(OutputFieldsDeclarer declarer) {
-			declarer.declare(new Fields("word"));
-		}
-
 	}
 }
--- src/main/java/world/we/deserve/StormCassandraComponentScan.java
+++ src/main/java/world/we/deserve/StormCassandraComponentScan.java
@@ -3,8 +3,12 @@
  */
 package world.we.deserve;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import backtype.storm.Config;
+import storm.trident.TridentTopology;
 
 /**
  * @author Miguel Ángel Dev (miguelangelprogramacion@gmail.com)
@@ -13,5 +17,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = { "world.we.deserve" })
 public class StormCassandraComponentScan {
+
+	@Bean
+	public TridentTopology cluster() {
+
+		TridentTopology topology = new TridentTopology();
+		return topology;
+	}
+
+	@Bean
+	public Config tridentConfig() {
+
+		// Remenber (limited config)
+		Config tridentConfig = new Config();
+		tridentConfig.setMessageTimeoutSecs(60);
+		tridentConfig.setNumAckers(1);
+		tridentConfig.setNumWorkers(1);
+		tridentConfig.setMaxSpoutPending(1);
+		tridentConfig.setMaxTaskParallelism(1);
+
+		tridentConfig.put(Config.TOPOLOGY_TRIDENT_BATCH_EMIT_INTERVAL_MILLIS, 1000);
+		tridentConfig.put(Config.TOPOLOGY_EXECUTOR_RECEIVE_BUFFER_SIZE, 16384);
+		tridentConfig.put(Config.TOPOLOGY_EXECUTOR_SEND_BUFFER_SIZE, 16384);
+		tridentConfig.put(Config.TOPOLOGY_TRANSFER_BUFFER_SIZE, 32);
+		tridentConfig.put(Config.TOPOLOGY_STATS_SAMPLE_RATE, 0.05);
+
+		// tridentConfig.put(MapConfiguredCqlClientFactory.TRIDENT_CASSANDRA_CQL_HOSTS,
+		// "localhost");
+
+		tridentConfig.setDebug(false);
+
+		return tridentConfig;
+	}
 
 }
