2a06bb9748c68f3b24117a691142a6c1d55b81b8
miguelangelprogramacion <miguelangelprogramacion@gmail.com>
2015-08-22 01:58:58 +0200

    Added cassandra bolts. Trying to inject Spring CassandraTemplate

--- src/main/java/world/we/deserve/App.java
+++ src/main/java/world/we/deserve/App.java
@@ -1,7 +1,5 @@
 package world.we.deserve;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,34 +7,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
-import backtype.storm.Config;
+
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
-import backtype.storm.LocalCluster;
-import backtype.storm.StormSubmitter;
 import backtype.storm.StormSubmitter;
 import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.AuthorizationException;
 import backtype.storm.generated.AuthorizationException;
 import backtype.storm.generated.InvalidTopologyException;
-import backtype.storm.generated.InvalidTopologyException;
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
-import backtype.storm.testing.TestWordSpout;
-import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.TopologyBuilder;
-import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Fields;
-import backtype.storm.tuple.Tuple;
-import backtype.storm.tuple.Values;
-import backtype.storm.utils.Utils;
 import backtype.storm.utils.Utils;
 import storm.trident.Stream;
-import storm.trident.Stream;
 import storm.trident.TridentTopology;
-import storm.trident.TridentTopology;
+import world.we.deserve.bolt.dao.CassandraCqlStateFactory;
+import world.we.deserve.bolt.dao.CassandraCqlStateUpdater;
+import world.we.deserve.bolt.dao.FileStreamMapper;
 import world.we.deserve.bolt.function.PrintTuple;
 import world.we.deserve.spout.LoadFileData;
 /**
@@ -57,26 +41,37 @@ public class App {
 	
 	@Autowired
 	PrintTuple printBolt;
+	
+	@Autowired
+	CassandraCqlStateFactory cassandraCqlStateFactory;
+	
+	@Autowired
+	CassandraCqlStateUpdater cassandraCqlStateUpdater;
+	
+	@Autowired
+	FileStreamMapper mapper;
 
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(StormCassandraComponentScan.class);
 		App app = context.getBean(App.class);
-//		app.greeting(args);
 
 		String host = null;
 		if (args != null && args.length > 0) {
 			host = args[0];
 		}
 		app.tridentLaunch(host);
-
 	}
 
 	private void tridentLaunch(String host) {
 
-		Stream fileStream = topology.newStream("test", loadFileDataSpout);		
-		fileStream.each(new Fields("test"), printBolt, new Fields());
+		Stream fileStream = topology.newStream("test", loadFileDataSpout);	
+		
+		fileStream.partitionPersist(cassandraCqlStateFactory, 
+				new Fields("test"), cassandraCqlStateUpdater);
+		
+		//fileStream.each(new Fields("test"), printBolt, new Fields());
 
 		if (host != null) {
 
--- src/main/java/world/we/deserve/StormCassandraComponentScan.java
+++ src/main/java/world/we/deserve/StormCassandraComponentScan.java
@@ -6,6 +6,16 @@ package world.we.deserve;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
+import org.springframework.data.cassandra.config.CassandraSessionFactoryBean;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.convert.CassandraConverter;
+import org.springframework.data.cassandra.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext;
+import org.springframework.data.cassandra.mapping.CassandraMappingContext;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 import backtype.storm.Config;
 import storm.trident.TridentTopology;
@@ -16,7 +26,14 @@ import storm.trident.TridentTopology;
  */
 @Configuration
 @ComponentScan(basePackages = { "world.we.deserve" })
+@EnableCassandraRepositories(basePackages = { "world.we.deserve.dao" })
 public class StormCassandraComponentScan {
+
+	/**************************************************************
+	**************************************************************
+						Storm trident config
+	**************************************************************
+	**************************************************************/
 
 	@Bean
 	public TridentTopology cluster() {
@@ -49,5 +66,49 @@ public class StormCassandraComponentScan {
 
 		return tridentConfig;
 	}
+	
+	/**************************************************************
+	**************************************************************
+						Cassandra - Config
+	**************************************************************
+	**************************************************************/
+	
+	 @Bean
+	  public CassandraClusterFactoryBean clusterCassandra() {
+
+	    CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
+	    cluster.setContactPoints("localhost");
+	    cluster.setPort(9042);
+
+	    return cluster;
+	  }
+
+	  @Bean
+	  public CassandraMappingContext mappingContext() {
+	    return new BasicCassandraMappingContext();
+	  }
+
+	  @Bean
+	  public CassandraConverter converter() {
+	    return new MappingCassandraConverter(mappingContext());
+	  }
+
+	  @Bean
+	  public CassandraSessionFactoryBean session() throws Exception {
+
+	    CassandraSessionFactoryBean session = new CassandraSessionFactoryBean();
+	    //TODO: DI?
+	    session.setCluster(clusterCassandra().getObject());
+	    session.setKeyspaceName("demo");
+	    session.setConverter(converter());
+	    session.setSchemaAction(SchemaAction.NONE);
+
+	    return session;
+	  }
+
+	  @Bean
+	  public CassandraOperations cassandraTemplate() throws Exception {
+	    return new CassandraTemplate(session().getObject());
+	  }
 
 }
